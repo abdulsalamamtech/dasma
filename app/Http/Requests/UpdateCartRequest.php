2b1d8d47ef2d5387cf68386c00e38cc5e6cdc85f
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-04-21 16:27:25 +0100

    Modified update cart request

--- app/Http/Requests/UpdateCartRequest.php
+++ app/Http/Requests/UpdateCartRequest.php
@@ -22,7 +22,7 @@ class UpdateCartRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'product_id' => ['required', 'exists:products,id'],
+            // 'product_id' => ['required', 'exists:products,id'],
             'quantity' => ['required', 'integer', 'min:1'],
         ];
     }
