5559cf9e35c1561954b1934241d45eb2e932fe9f
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-11-05 14:32:33 +0100

    Modified user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,11 +11,12 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, 
-        HasRoles, MustVerifyEmail,
+        HasRoles, 
+        // MustVerifyEmail,
         SoftDeletes;
 
     /**
