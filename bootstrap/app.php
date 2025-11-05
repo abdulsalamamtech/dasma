6527d3c5f0f2ca04ec7daf38d49a12d54394316e
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-11-05 13:01:42 +0100

    Modified middleware alias

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,12 +6,28 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // Add your middleware here
+
+        // Custom middleware
+        $middleware->alias([
+
+            // Users must have verified email to access these routes
+            // 'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
+
+            // Comment this when using spatie permission
+            // 'admin' => AdminMiddleware::class,
+            // 'user-role' => \App\Http\Middleware\UserRoleMiddleware::class,
+
+            // This is for laravel spatie/laravel-permission
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
