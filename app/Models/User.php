c18c03f41a29d4e2660920e55e174bf8da645195
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-11-05 14:35:53 +0100

    Modified user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,9 +2,9 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 
-use Illuminate\Auth\MustVerifyEmail;
+// use Illuminate\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
